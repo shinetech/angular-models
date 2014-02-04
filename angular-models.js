71dbe767dde256b3c039bbef76b8b482d3fc511b
Ben Teese <ben.teese@gmail.com>
2014-02-04 12:55:44 +1100

    Added documentation

--- angular-models.js
+++ angular-models.js
@@ -4,10 +4,23 @@
 
 angular.module('shinetech.models', []).factory('Base', function() {
   return {
+    /**
+     * Defines a new mixin with a set of properties. Multiple sets of properties can be provided.
+     * If two property sets define the same property name, the last one will take priority.
+     *
+     * Mixins can extend upon each other.
+     */
     extend: function() {
       var args = Array.prototype.slice.call(arguments);
       return angular.extend.apply(null, [{}, this].concat(args));
     },
+    /**
+     * Mixes the properties of this mixin into an object. If the mixin defines a beforeMixingInto
+     * method, that will get called _before_ the mixing occurs.
+     *
+     * The first argument is the object to mix into. This will also be passed to beforeMixingInto.
+     * If any subsequent arguments are provided, they will also be passed to beforeMixingInto.
+     */
     mixInto: function() {
       var object = arguments[0];
       // If we're actually mixing into something,
