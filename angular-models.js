f501b08c89e7915ae620f6dce56d6b4e1e7ebff0
Ben Teese <ben.teese@gmail.com>
2014-02-26 16:01:41 +1100

    Added comments for the base mixin

--- angular-models.js
+++ angular-models.js
@@ -34,40 +34,46 @@ angular.module('shinetech.models', []).factory('extend',
       return dst;
     };
   }
-).factory('Base', function(extend) {
-  return {
-    /**
-     * Defines a new mixin with a set of properties. Multiple sets of properties can be provided.
-     * If two property sets define the same property name, the last one will take priority.
-     *
-     * Mixins can extend upon each other.
-     */
-    extend: function() {
-      var args = Array.prototype.slice.call(arguments);
-      return extend.apply(null, [{}, this].concat(args));
-    },
-    /**
-     * Mixes the properties of this mixin into an object. If the mixin defines a beforeMixingInto
-     * method, that will get called _before_ the mixing occurs.
-     *
-     * The first argument is the object to mix into. This will also be passed to beforeMixingInto.
-     * If any subsequent arguments are provided, they will also be passed to beforeMixingInto.
-     */
-    mixInto: function() {
-      var object = arguments[0];
-      // If we're actually mixing into something,
-      if (object) {
-        // If we've got some mixing customization todo, then invoke it
-        if (this.beforeMixingInto) this.beforeMixingInto.apply(this, arguments);
+).factory('Base',
+  /**
+   * A base mixin that other mixins can extend upon. Provides basic infrastructure for defining new
+   * mixins (`extend`) and mixing them into objects (`mixInto`).
+   */
+  function(extend) {
+    return {
+      /**
+       * Defines a new mixin with a set of properties. Multiple sets of properties can be provided.
+       * If two property sets define the same property name, the last one will take priority.
+       *
+       * Mixins can extend upon each other.
+       */
+      extend: function() {
+        var args = Array.prototype.slice.call(arguments);
+        return extend.apply(null, [{}, this].concat(args));
+      },
+      /**
+       * Mixes the properties of this mixin into an object. If the mixin defines a beforeMixingInto
+       * method, that will get called _before_ the mixing occurs.
+       *
+       * The first argument is the object to mix into. This will also be passed to beforeMixingInto.
+       * If any subsequent arguments are provided, they will also be passed to beforeMixingInto.
+       */
+      mixInto: function() {
+        var object = arguments[0];
+        // If we're actually mixing into something,
+        if (object) {
+          // If we've got some mixing customization todo, then invoke it
+          if (this.beforeMixingInto) this.beforeMixingInto.apply(this, arguments);
 
-        // Always do this
-        extend(object, this);
+          // Always do this
+          extend(object, this);
 
-        return object;
+          return object;
+        }
       }
-    }
-  };
-}).factory('identityMap',
+    };
+  }
+).factory('identityMap',
     /**
      * A simple identity-map implementation. This can be used to ensure that, for some class
      * descriptor and ID, only one instance of a particular object is ever used.
